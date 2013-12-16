4ec8c88634520a65fac1ba637fb688cfbb4f07f4
Benjamin W. Smith <benjaminwarfield@just-another.net>
2013-12-15 21:36:48 -0800

    Add gitignore. Add py.test. Add silly first test.

---
+++ tests/test_logstash_index_cleaner.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('.')
+
+from logstash_index_cleaner import get_index_epoch, find_expired_indices, find_overusage_indices 
+
+def test_get_index_epoch():
+    """ This guy isn't very flexible :( """
+    unprefixed_index_name = '013.12.16'
+    assert get_index_epoch(unprefixed_index_name) == 1387191600.0
